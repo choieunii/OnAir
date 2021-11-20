6c178573c90c4043cdee77af25a3147ee8a2d2bb
pyr53540 <pyr53540@gmail.com>
2021-11-20 21:30:06 +0900

    flight 변경

--- src/main/kotlin/com/example/onair/domain/AirportCode/AirportCodeRepository.kt
+++
@@ -1,5 +0,0 @@
-package com.example.onair.domain.AirportCode
-
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface AirportCodeRepository : JpaRepository<AirportCode, String>
--- src/main/kotlin/com/example/onair/domain/flight/Flight.kt
+++ src/main/kotlin/com/example/onair/domain/flight/Flight.kt
@@ -9,16 +9,16 @@ import javax.persistence.Table
 @Table(name = "flight")
 data class Flight(
         @Id
-        @Column(name = "schDate") // 검색일자
-        var schDate: String = "",
-        @Column(name = "schDeptCityCode") // 도착 도시 코드
-        var schDeptCityCode: String = "",
-        @Column(name = "schArrvCityCode") // 출항 도시 코드
-        var schArrvCityCode: String = "",
-        @Column(name = "schAirLine") // 	항공편 코드
-        var schAirLine: String = "",
-        @Column(name = "schFlightNum") // 항공편 넘버
-        var schFlightNum: String = "",
-        @Column(name = "serviceKey") // 인증키
-        var serviceKey: String = "yte0lqvUBjmStje3Bv6YEA5dectrmAum%2BiBn%2FCK3vg3OZo1NTSaI%2BFpfJYfuA5%2FO3Q6VXBMMMUlmAbCZnIaBVA%3D%3D"
+        @Column(name = "FlightNum") //
+        var FlightNum: Int = -1,
+        @Column(name = "DepartmentAirport") //
+        var DepartmentAirport: String = "",
+        @Column(name = "ArriveAirport") //
+        var ArriveAirport: String = "",
+        @Column(name = "EconomyCharge") //
+        var EconomyCharge: Int = -1,
+        @Column(name = "BusinessCharge") //
+        var BusinessCharge: Int = -1,
+        @Column(name = "FirstCharge") //
+        var FirstCharge: Int = -1
 )
