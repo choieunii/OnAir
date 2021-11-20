87bb7ed78b59522d01b8c2a61256bb2d85dd3c1a
choieunii <ghk784@dgu.ac.kr>
2021-11-20 21:30:30 +0900

    Fix : white label error

--- src/main/kotlin/com/example/onair/controller/UserController.kt
+++ src/main/kotlin/com/example/onair/controller/UserController.kt
@@ -6,32 +6,32 @@ import com.example.onair.config.KakaoOauth
 import com.example.onair.dto.SignUpRequestDto
 import com.example.onair.service.UserService
 import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.*
 import javax.servlet.http.HttpServletRequest
 
 
 @Controller
 class UserController (private val userService: UserService){
-    @RequestMapping("/login")
+    @GetMapping("/login")
+    fun login(): String {
+        return "login";
+    } // 세션 등록, 포인트 변수 넣기 = 0,
+    @PostMapping("/login")
     fun login(id: String, password: String): String {
         val response = userService.login(id,password)
         println(response)
         if(response.equals("Success")){
             return "index"
-        }else{
-            return "login";
         }
+        return "login";
     } // 세션 등록, 포인트 변수 넣기 = 0,
     @RequestMapping("/signup")
     fun signUp(request : SignUpRequestDto): String {
         val response = userService.signUp(request)
         if(response.equals("Success")){
             return "login"
-        }else{
-            return "signup";
         }
+        return "signup";
     }
 
     @RequestMapping("/login/google")
