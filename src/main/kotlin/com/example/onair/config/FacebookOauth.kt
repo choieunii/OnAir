cc091b6905b67f991055a2e5e2d810d1a11e26f0
choieunii <ghk784@dgu.ac.kr>
2021-11-20 20:15:44 +0900

    Feat : Facebook 로그인

---
+++ src/main/kotlin/com/example/onair/config/FacebookOauth.kt
@@ -0,0 +1,48 @@
+package com.example.onair.config
+
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpMethod
+import org.springframework.web.client.RestTemplate
+import org.springframework.web.util.UriComponentsBuilder
+
+class FacebookOauth(
+        val facebookLoginUrl: String = "",
+        val facebookClientId: String = "",
+        val facebookRedirectUrl: String = "",
+        val facebookClientSecret: String = "",
+        val facebookTokenBaseUrl: String = ""
+) {
+
+    fun getOauthRedirectURL(): String {
+        return facebookLoginUrl.toString() + "?client_id=" + facebookClientId + "&redirect_uri=" + facebookRedirectUrl
+    }
+
+    fun requestAccessToken(code: String): String {
+        val restTemplate = RestTemplate();
+        val uri = UriComponentsBuilder.fromHttpUrl(facebookTokenBaseUrl)
+                .queryParam("client_id", facebookClientId)
+                .queryParam("redirect_uri", facebookRedirectUrl)
+                .queryParam("client_secret", facebookClientSecret)
+                .queryParam("code", code);
+
+        val apiResponse = restTemplate.getForEntity(uri.build().toUri(), Map::class.java)
+        val responseBody = apiResponse.getBody()
+        println(responseBody)
+        val token = responseBody?.get("access_token").toString();
+
+        return token;
+    }
+
+    fun getFacebookUserInfo(token: String): String {
+        val restTemplate = RestTemplate();
+        val requestUrl = "https://graph.facebook.com/me";
+        val uri = UriComponentsBuilder.fromHttpUrl(requestUrl)
+                .queryParam("fields", "id,name,email,picture,birthday")
+                .queryParam("access_token", token)
+        val response = restTemplate.getForEntity(uri.build().toUri(), Map::class.java)
+        val body = response.getBody()
+        println(body)
+        return "";
+    }
+}
