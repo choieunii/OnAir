49b265ebb05b17fc2a3519821dba9b36a8135cd9
hojong <33508056+LteFroggy@users.noreply.github.com>
2021-11-22 17:18:21 +0900

    FlightRepository 기능 추가

--- src/main/kotlin/com/example/onair/domain/flight/FlightRepository.kt
+++ src/main/kotlin/com/example/onair/domain/flight/FlightRepository.kt
@@ -1,7 +1,17 @@
-package com.example.onair.domain.flight
+package com.example.onair.domain.Flight
 
+import com.example.onair.domain.flight.Flight
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 
 @Repository
-interface FlightRepository : JpaRepository<Flight, String>
+interface FlightRepository : JpaRepository<Flight, String>{
+    @Query("select * from Flight")
+    fun findFlight(): List<Flight>
+    @Query("SELECT FlightNum FROM Flight WHERE DepartmentAirport = %?1% AND ArriveAirport = %?2% AND DepartmentDate = %?3%")
+    fun getFlightNum(depAirport : String, arrAirport : String, dDate : String) : Int
+    @Query("SELECT %?1% FROM Flight WHERE FlightNum = %?2%")
+    fun getCharge(SeatType : String, FlightNum : Int) : Int
+
+}
