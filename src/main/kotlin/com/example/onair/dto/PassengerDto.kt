a6ba2c021eb2c6e0e6d10c3ed747ce10e0425a84
hojong <33508056+LteFroggy@users.noreply.github.com>
2021-11-22 17:19:34 +0900

    PassengerDto 추가

---
+++ src/main/kotlin/com/example/onair/dto/PassengerDto.kt
@@ -0,0 +1,11 @@
+package com.example.onair.dto
+
+data class PassengerDto (
+    var gender : String,
+    var lastName : String,
+    var firstName : String,
+    var year: Int,
+    var month : Int,
+    var day : Int,
+    var airline : String
+)
