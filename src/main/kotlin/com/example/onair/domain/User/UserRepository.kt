f8e5e601f5c76c59d2598e6433dd755817696ed6
hojong <33508056+LteFroggy@users.noreply.github.com>
2021-11-22 17:13:45 +0900

    UserRepository 기능 추가

--- src/main/kotlin/com/example/onair/domain/User/UserRepository.kt
+++ src/main/kotlin/com/example/onair/domain/User/UserRepository.kt
@@ -1,8 +1,13 @@
 package com.example.onair.domain.User
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 
 interface UserRepository: JpaRepository<User, Long> {
     fun findByUserId(user_id: String): User?
     fun findByEmail(email: String): User?
+    @Query("UPDATE USER SET balance = %?1% WHERE user_id = %?2%")
+    fun setBalance(newValue : Long, userId : String)
+    @Query("SELECT balance FROM USER WHERE user_id = %?1%")
+    fun getBalance(user_id : String) : Long
 }
