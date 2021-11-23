ae602784789cdee9cc46939946dba08d0639027f
choieunii <ghk784@dgu.ac.kr>
2021-11-24 01:25:31 +0900

    Merge : flight branch

---
+++ src/main/kotlin/com/example/onair/domain/flightInfo/Test.kt
@@ -0,0 +1,65 @@
+package com.example.onair.domain.test
+
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.Id
+import javax.persistence.Table
+@Entity
+@Table(name = "FLIGHT_INFORMATION")
+data class FlightInformation (
+        @Id
+        @Column(name = "FlightNum")
+        var FlightNum : Number = 0,
+        @Column(name = "DepartmentAirport")
+        var DepartmentAirport : String = "",
+        @Column(name = "ArriveAirport")
+        var ArriveAirport : String = "",
+        @Column(name = "DepartmentTime")
+        var DepartmentTime : Number = 0,
+        @Column(name = "ArriveTime")
+        var ArriveTime : Number = 0,
+        @Column(name = "EconomyCharge")
+        var EconomyCharge : Number = 0,
+        @Column(name = "PrestigeCharge")
+        var PrestigeCharge : Number = 0
+)
+
+@Entity
+@Table(name = "CUSTOMER")
+data class Customer (
+        @Id
+        @Column(name = "CustomerID")
+        var CustomerID : Number = 0,
+        @Column(name = "LoginID")
+        var LoginID : String = "",
+        @Column(name = "Password")
+        var Password : String = "",
+        @Column(name = "Name")
+        var Name : String = "",
+        @Column(name = "Age")
+        var Age : Number = 0,
+        @Column(name = "Balance")
+        var Balance : Number = 0,
+        @Column(name = "Phone")
+        var Phone : String = "",
+        @Column(name = "Email")
+        var Email : String = "",
+        @Column(name = "Address")
+        var Address : String = ""
+)
+
+@Entity
+@Table(name = "BOOK_INFO")
+data class BookInfo (
+        @Id
+        @Column(name = "BookID")
+        var BookID : Number = 0,
+        @Column(name = "CustomerID")
+        var CustomerID : Number = 0,
+        @Column(name = "FlightNum")
+        var FlightNum : Number = 0,
+        @Column(name = "Headcount")
+        var Headcount : Number = 0,
+        @Column(name = "SeatClass")
+        var SeatClass : String = ""
+)
