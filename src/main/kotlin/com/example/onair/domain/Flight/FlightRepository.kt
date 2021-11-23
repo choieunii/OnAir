2f008904a6b6b2a1b885c466d0c3f7b6c20abafc
choieunii <ghk784@dgu.ac.kr>
2021-11-24 01:33:07 +0900

    Fix : Flight

--- src/main/kotlin/com/example/onair/domain/Flight/FlightRepository.kt
+++ src/main/kotlin/com/example/onair/domain/Flight/FlightRepository.kt
@@ -1,7 +1,16 @@
 package com.example.onair.domain.flight
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 
 @Repository
-interface FlightRepository : JpaRepository<Flight, String>
+interface FlightRepository : JpaRepository<Flight, Int> {
+    @Query("SELECT f FROM Flight f WHERE f.departmentAirport = :departAirport and f.arriveAirport = :arrAirport")
+    fun getFlightOnAirInfo(departAirport: String, arrAirport: String): List<Flight>?
+
+    @Query("SELECT f FROM Flight f WHERE f.departmentAirport = :depAirport AND f.arriveAirport = :arrAirport AND f.departmentDate = :dDate")
+    fun getFlightNum(depAirport : String, arrAirport : String, dDate : String) : List<Flight>?
+
+    fun findInfoByFlightNum(flightNum: Int): Flight?
+}
--- src/main/kotlin/com/example/onair/domain/flight/Flight.kt
+++ src/main/kotlin/com/example/onair/domain/flight/Flight.kt
@@ -9,18 +9,55 @@ import javax.persistence.Table
 @Table(name = "flight")
 data class Flight( // data class -> equals, copy, toString, componentN
         @Id
-        @Column(name = "FlightNum", nullable=false) //
-        var FlightNum: Int,
-        @Column(name = "DepartmentAirport", nullable=false) //
-        var DepartmentAirport: String,
-        @Column(name = "ArriveAirport", nullable=false) //
-        var ArriveAirport: String,
-        @Column(name = "DepartmentDate", nullable = false) //
-        var DepartmentDate: String,
-        @Column(name = "EconomyCharge", nullable=false) //
-        var EconomyCharge: Int,
-        @Column(name = "BusinessCharge", nullable=false) //
-        var BusinessCharge: Int,
-        @Column(name = "FirstCharge", nullable=false) //
-        var FirstCharge: Int
-)
+        @Column(name = "flight_num", nullable=false) //
+        var flightNum: Int,
+        @Column(name = "department_airport", nullable=false) //
+        var departmentAirport: String,
+        @Column(name = "arrive_airport", nullable=false) //
+        var arriveAirport: String,
+        @Column(name = "department_date", nullable = false) //
+        var departmentDate: String,
+        @Column(name = "arrive_date", nullable = false) //
+        var arriveDate: String,
+        @Column(name = "department_time", nullable = false) //
+        var departmentTime: String,
+        @Column(name = "arrive_time", nullable = false) //
+        var arriveTime: String,
+        @Column(name = "economy_charge", nullable=false) //
+        var economyCharge: Int,
+        @Column(name = "business_charge", nullable=false) //
+        var businessCharge: Int,
+        @Column(name = "first_charge", nullable=false) //
+        var firstCharge: Int
+){
+        fun getFN():Int{
+                return flightNum;
+        }
+        fun getDMA():String{
+                return departmentAirport;
+        }
+        fun getAA():String{
+                return arriveAirport;
+        }
+        fun getDD():String{
+                return departmentDate;
+        }
+        fun getAD():String{
+                return arriveDate;
+        }
+        fun getDT():String{
+                return departmentTime;
+        }
+        fun getAT():String{
+                return arriveTime;
+        }
+        fun getEC():Int{
+                return economyCharge;
+        }
+        fun getBC():Int{
+                return businessCharge;
+        }
+        fun getFC():Int{
+                return firstCharge;
+        }
+}
