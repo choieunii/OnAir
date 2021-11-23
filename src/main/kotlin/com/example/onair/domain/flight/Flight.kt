418eb8776d7ea3c49a30eb3d0763204821de1547
choieunii <ghk784@dgu.ac.kr>
2021-11-24 00:31:51 +0900

    Chore : Front 요청에 맞춰 수정

--- src/main/kotlin/com/example/onair/domain/flight/Flight.kt
+++ src/main/kotlin/com/example/onair/domain/flight/Flight.kt
@@ -9,18 +9,42 @@ import javax.persistence.Table
 @Table(name = "flight")
 data class Flight( // data class -> equals, copy, toString, componentN
         @Id
-        @Column(name = "FlightNum", nullable=false) //
-        var FlightNum: Int,
-        @Column(name = "DepartmentAirport", nullable=false) //
-        var DepartmentAirport: String,
-        @Column(name = "ArriveAirport", nullable=false) //
-        var ArriveAirport: String,
-        @Column(name = "DepartmentDate", nullable = false) //
-        var DepartmentDate: String,
-        @Column(name = "EconomyCharge", nullable=false) //
-        var EconomyCharge: Int,
-        @Column(name = "BusinessCharge", nullable=false) //
-        var BusinessCharge: Int,
-        @Column(name = "FirstCharge", nullable=false) //
-        var FirstCharge: Int
-)
+        @Column(name = "flight_num", nullable=false) //
+        var flightNum: Int,
+        @Column(name = "department_airport", nullable=false) //
+        var departmentAirport: String,
+        @Column(name = "arrive_airport", nullable=false) //
+        var arriveAirport: String,
+        @Column(name = "department_time", nullable = false) //
+        var departmentTime: String,
+        @Column(name = "arrive_time", nullable = false) //
+        var arriveTime: String,
+        @Column(name = "economy_charge", nullable=false) //
+        var economyCharge: Int,
+        @Column(name = "business_charge", nullable=false) //
+        var businessCharge: Int,
+        @Column(name = "first_charge", nullable=false) //
+        var firstCharge: Int
+){
+    fun getFN():Int{
+        return flightNum;
+    }
+    fun getDMA():String{
+        return departmentAirport;
+    }
+    fun getAA():String{
+        return arriveAirport;
+    }
+    fun getDD():String{
+        return departmentTime;
+    }
+    fun getEC():Int{
+        return economyCharge;
+    }
+    fun getBC():Int{
+        return businessCharge;
+    }
+    fun getFC():Int{
+        return firstCharge;
+    }
+}
